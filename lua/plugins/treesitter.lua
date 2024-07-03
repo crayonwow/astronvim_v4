7f3015224890ac422a0a5d3cf5b087029e361eaa
Stepan Martynovskiy <s.martynovskiy@space307.com>
2024-07-03 15:48:46 +0300

    remote debug go

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
       "rust",
       "python",
       -- add more arguments for adding more treesitter parsers
+      highlight = { enable = false },
     })
   end,
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -71,4 +71,61 @@ return {
     config = function() require("telescope").load_extension "goimpl" end,
   },
   { "goolord/alpha-nvim", enabled = false },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    opts = {},
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "delve" })
+        end,
+      },
+    },
+    config = function()
+      require("dap-go").setup {
+        dap_configurations = {
+          {
+            type = "go",
+            name = "Attach remote",
+            mode = "remote",
+            request = "attach",
+            port = 38697,
+            host = "localhost",
+          },
+        },
+        delve = {
+          -- the path to the executable dlv which will be used for debugging.
+          -- by default, this is the "dlv" executable on your PATH.
+          path = "dlv",
+          -- time to wait for delve to initialize the debug session.
+          -- default to 20 seconds
+          initialize_timeout_sec = 20,
+          -- a string that defines the port to start delve debugger.
+          -- default to string "${port}" which instructs nvim-dap
+          -- to start the process in a random available port
+          port = 38697,
+          -- additional args to pass to dlv
+          args = {},
+          -- the build flags that are passed to delve.
+          -- defaults to empty string, but can be used to provide flags
+          -- such as "-tags=unit" to make sure the test suite is
+          -- compiled during debugging, for example.
+          -- passing build flags using args is ineffective, as those are
+          -- ignored by delve in dap mode.
+          build_flags = "",
+          -- whether the dlv process to be created detached or not. there is
+          -- an issue on Windows where this needs to be set to false
+          -- otherwise the dlv server creation will fail.
+          detached = true,
+          -- the current working directory to run dlv from, if other than
+          -- the current working directory.
+          cwd = nil,
+        },
+      }
+    end,
+  },
 }
