b79741605106175e9133a88b118d5742ec98cd31
Stepan Martynovskiy <s.martynovskiy@space307.com>
2024-08-23 19:55:45 +0300

    neotest for go

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -14,39 +14,39 @@ return {
   },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "fredrikaverpil/neotest-golang", -- Installation
+    },
     config = function()
-      -- get neotest namespace (api call creates or returns namespace)
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
       require("neotest").setup {
-        -- your neotest config here
         adapters = {
-          require "neotest-go" {
-            runner = "testify",
-            experimental = {
-              test_table = true,
-            },
-            args = { "-count=1", "-timeout=30s", "-race" },
-          },
-          require "neotest-rust",
-          require "neotest-python",
+          require "neotest-golang", -- Registration
         },
       }
     end,
-    dependencies = {
-      "nvim-neotest/neotest-go",
-      "nvim-neotest/neotest-python",
-      "rouge8/neotest-rust",
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
+    opts = {
+      -- See all config options with :h neotest.Config
+      discovery = {
+        -- Drastically improve performance in ginormous projects by
+        -- only AST-parsing the currently opened buffer.
+        enabled = false,
+        -- Number of workers to parse files concurrently.
+        -- A value of 0 automatically assigns number based on CPU.
+        -- Set to 1 if experiencing lag.
+        concurrent = 1,
+      },
+      running = {
+        -- Run tests concurrently when an adapter provides multiple commands to run.
+        concurrent = true,
+      },
+      summary = {
+        -- Enable/disable animation of icons.
+        animated = false,
+      },
     },
     keys = {
       { "<Leader>t", mode = { "n" }, desc = "Test" },
